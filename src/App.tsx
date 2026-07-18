3a77b7ce0e08d0d037027b6f541e82a516625ad0
akshy-yy <akshaya.ixa@gmail.com>
2026-07-18 23:16:00 +0530

    Fixed Errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CameraCapture from './pages/CameraCapture';
 import Customize from './pages/Customize';
